a69f54322b20816ecbdb54812ef26475eedebd69
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-08-02 10:33:01 -0700

    submission for deadline

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask import Flask, jsonify, request, make_response
 from flask_restful import Api, Resource
 import os
-
+    
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
